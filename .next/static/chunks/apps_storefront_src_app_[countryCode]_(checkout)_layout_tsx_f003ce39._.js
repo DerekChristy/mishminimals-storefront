83af1b22743c2d875100b206b2d782bf1eb84f39
DerekChristy <kered.christy@gmail.com>
2026-05-19 01:43:03 +0530

    chore: clear path tracking cache and enforce strict lowercase paths

--- .next/static/chunks/apps_storefront_src_app_[countryCode]_(checkout)_layout_tsx_f003ce39._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_c26829d9._.js"
-],
-    source: "dynamic"
-});
--- .next/transform.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "transform.js";
-const runtime = require("./build/chunks/[turbopack]_runtime.js");
-runtime.loadChunk("build/chunks/apps_storefront_postcss_config_js_transform_ts_aa0b16cc._.js");
-runtime.loadChunk("build/chunks/[root-of-the-server]__63e623d5._.js");
-runtime.getOrInstantiateRuntimeModule("[turbopack-node]/globals.ts [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/apps/storefront/postcss.config.js/transform.ts { CONFIG => \\\"[project]/apps/storefront/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/apps/storefront/postcss.config.js/transform.ts { CONFIG => \\\"[project]/apps/storefront/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
